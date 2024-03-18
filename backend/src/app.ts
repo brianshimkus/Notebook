b5bfae1d299c6b23b837b79efc4bd796135381a5
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-17 23:46:10 -0500

    Error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,24 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { Request, Response } from 'express'
 import NoteModel from './models/note'
 
 const app = express()
 
-app.get('/', async (req, res) => {
-	const notes = await NoteModel.find().exec()
-	res.status(200).json(notes)
+app.get('/', async (req, res, next) => {
+	try {
+		// throw new Error('Error!')
+		const notes = await NoteModel.find().exec()
+		res.status(200).json(notes)
+	} catch (err) {
+		next(err)
+	}
+})
+
+app.use((err: unknown, req: Request, res: Response) => {
+	console.log(err)
+	let errorMessage = 'An unknown error occurred'
+	if (err instanceof Error) errorMessage = err.message
+	res.status(500).json({ message: errorMessage })
 })
 
 export default app
