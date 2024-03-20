92d7a4ac605e0d914100117c18581ba80e188c4b
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-19 23:50:33 -0500

    Post new note functionality

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -10,3 +10,16 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 		next(err)
 	}
 }
+
+export const createNote: RequestHandler = async (req, res, next) => {
+	const title = req.body.title
+	const text = req.body.text
+
+	try {
+		const newNote = await NoteModel.create({ title, text })
+
+		res.status(201).json(newNote)
+	} catch (err) {
+		next(err)
+	}
+}
