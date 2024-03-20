617372fb59f08b16fe93cfacd9bd53aec6b47cd1
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-19 23:55:40 -0500

    Get individual note

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -11,6 +11,23 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 	}
 }
 
+export const getNote: RequestHandler = async (req, res, next) => {
+	const noteId = req.params.id
+
+	try {
+		const note = await NoteModel.findById(noteId).exec()
+
+		if (!note) {
+			res.status(404).json({ message: 'Note not found' })
+			return
+		}
+
+		res.status(200).json(note)
+	} catch (err) {
+		next(err)
+	}
+}
+
 export const createNote: RequestHandler = async (req, res, next) => {
 	const title = req.body.title
 	const text = req.body.text
