1bf68a4fb4507260714f355fb57b6d2ae040a0ac
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-21 18:46:28 -0500

    Update note functionality

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -14,7 +14,7 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 }
 
 export const getNote: RequestHandler = async (req, res, next) => {
-	const noteId = req.params.id
+	const noteId = req.params.noteId
 
 	try {
 		if (!mongoose.isValidObjectId(noteId)) {
@@ -54,6 +54,52 @@ export const createNote: RequestHandler<
 		const newNote = await NoteModel.create({ title, text })
 
 		res.status(201).json(newNote)
+	} catch (err) {
+		next(err)
+	}
+}
+
+interface UpdateNoteParams {
+	noteId: string
+}
+
+interface UpdateNoteBody {
+	title?: string
+	text?: string
+}
+
+export const updateNote: RequestHandler<
+	UpdateNoteParams,
+	unknown,
+	UpdateNoteBody,
+	unknown
+> = async (req, res, next) => {
+	const noteId = req.params.noteId
+	const newTitle = req.body.title
+	const newText = req.body.text || ''
+
+	try {
+		if (!mongoose.isValidObjectId(noteId)) {
+			throw createHttpError(400, 'Invalid note ID')
+		}
+
+		if (!newTitle) {
+			throw createHttpError(400, 'Note must have a title')
+		}
+
+		const note = await NoteModel.findById(noteId).exec()
+
+		if (!note) {
+			throw createHttpError(404, 'Note not found')
+		}
+
+		note.title = newTitle
+		note.text = newText
+
+		const updatedNote = await note.save()
+
+		res.status(200).json(updatedNote)
+		NoteModel.findByIdAndUpdate()
 	} catch (err) {
 		next(err)
 	}
