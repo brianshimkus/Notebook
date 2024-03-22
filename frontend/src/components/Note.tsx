1914ce46cd013d1bf2011764f8aa3a51838278b9
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-22 18:59:21 -0500

    Styled and displayed notes

---
+++ frontend/src/components/Note.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/Note.module.css'
+import { Card } from 'react-bootstrap'
+import { Note as NoteModel } from '../models/note'
+
+interface NoteProps {
+	note: NoteModel
+	className?: string
+}
+
+export default function Note({ note, className }: NoteProps) {
+	const { title, text, createdAt, updatedAt } = note
+	return (
+		<Card className={`${styles.noteCard} ${className}`}>
+			<Card.Body className={styles.cardBody}>
+				<Card.Title>{title}</Card.Title>
+				<Card.Text className={styles.cardText}>{text}</Card.Text>
+			</Card.Body>
+			<Card.Footer className={styles.cardFooter}>{createdAt}</Card.Footer>
+		</Card>
+	)
+}
