f7ce06bc8ac63b7eff79a7466814c5a768d3da0c
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-22 19:08:19 -0500

    Create format date utility

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Note.module.css'
 import { Card } from 'react-bootstrap'
 import { Note as NoteModel } from '../models/note'
+import { formateDate } from '../utils/formateDate'
 
 interface NoteProps {
 	note: NoteModel
@@ -9,13 +10,23 @@ interface NoteProps {
 
 export default function Note({ note, className }: NoteProps) {
 	const { title, text, createdAt, updatedAt } = note
+
+	let createdUpdatedText: string
+	if (updatedAt > createdAt) {
+		createdUpdatedText = 'Updated: ' + formateDate(updatedAt)
+	} else {
+		createdUpdatedText = 'Created: ' + formateDate(createdAt)
+	}
+
 	return (
 		<Card className={`${styles.noteCard} ${className}`}>
 			<Card.Body className={styles.cardBody}>
 				<Card.Title>{title}</Card.Title>
 				<Card.Text className={styles.cardText}>{text}</Card.Text>
 			</Card.Body>
-			<Card.Footer className={styles.cardFooter}>{createdAt}</Card.Footer>
+			<Card.Footer className={styles.cardFooter}>
+				{createdUpdatedText}
+			</Card.Footer>
 		</Card>
 	)
 }
