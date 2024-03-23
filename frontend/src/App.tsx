02be3fde4f867315ebd3cf0e2cd990043548689c
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-22 20:08:41 -0500

    Create utility file for making API calls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Note as NoteModel } from './models/note'
 import Note from './components/Note'
 import { Col, Container, Row } from 'react-bootstrap'
 import styles from './styles/NotesPage.module.css'
+import * as NotesApi from './network/notes_api'
 
 function App() {
 	const [notes, setNotes] = useState<NoteModel[]>([])
@@ -10,10 +11,7 @@ function App() {
 	useEffect(() => {
 		async function loadNotes() {
 			try {
-				const res = await fetch('/api/notes', {
-					method: 'GET',
-				})
-				const notes = await res.json()
+				const notes = await NotesApi.fetchNotes()
 				setNotes(notes)
 			} catch (err) {
 				console.error(err)
