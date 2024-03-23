af678d27867c0c74eca50ab1759cf52aab4c1465
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-23 14:48:33 -0500

    Get authenticated user

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -3,6 +3,23 @@ import createHttpError from 'http-errors'
 import UserModel from '../models/user'
 import bcrypt from 'bcrypt'
 
+export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
+	const authenticatedUserId = req.session.userId
+
+	try {
+		if (!authenticatedUserId) {
+			throw createHttpError(404, 'User not authenticated')
+		}
+
+		const user = await UserModel.findById(authenticatedUserId)
+			.select('email')
+			.exec()
+		res.status(200).json(user)
+	} catch (error) {
+		next(error)
+	}
+}
+
 interface SignUpBody {
 	username?: string
 	email?: string
