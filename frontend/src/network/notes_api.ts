d39b8b1dfe5bfb7fbb0c2686de2ae4f6f26ec0d1
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-23 14:58:43 -0500

    Sign up and login functions

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -1,4 +1,5 @@
 import { Note } from '../models/note'
+import { User } from '../models/user'
 
 async function fetchData(input: RequestInfo, init?: RequestInit) {
 	const res = await fetch(input, init)
@@ -9,6 +10,48 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
 		const errorMessage = errorBody.err
 		throw Error(errorMessage)
 	}
+}
+
+export async function getLoggedInUser(): Promise<User> {
+	const res = await fetchData('/api/users', { method: 'GET' })
+	return res.json()
+}
+
+export interface SignUpCredentials {
+	username: string
+	email: string
+	password: string
+}
+
+export async function signUp(credentials: SignUpCredentials): Promise<User> {
+	const res = await fetchData('/api/users/signup', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(credentials),
+	})
+	return res.json()
+}
+
+export interface LoginCredentials {
+	username: string
+	password: string
+}
+
+export async function login(credentials: LoginCredentials): Promise<User> {
+	const response = await fetchData('/api/users/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(credentials),
+	})
+	return response.json()
+}
+
+export async function logout() {
+	await fetchData('/api/users/logout', { method: 'POST' })
 }
 
 export async function fetchNotes(): Promise<Note[]> {
@@ -38,14 +81,14 @@ export async function updateNote(
 	noteId: string,
 	note: NoteInput
 ): Promise<Note> {
-	const response = await fetchData('/api/notes/' + noteId, {
+	const res = await fetchData('/api/notes/' + noteId, {
 		method: 'PATCH',
 		headers: {
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify(note),
 	})
-	return response.json()
+	return res.json()
 }
 
 export async function deleteNote(noteId: string) {
