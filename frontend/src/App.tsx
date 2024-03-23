085585f9126aa2ae490773e22ee1c5ef2172a7e8
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-23 15:14:26 -0500

    Login modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import * as NotesApi from './network/notes_api'
 import AddEditNoteDialog from './components/AddEditNoteDialog'
 import { FaPlus } from 'react-icons/fa'
 import SignUpModal from './components/SignUpModal'
+import LoginModal from './components/LoginModal'
 
 function App() {
 	const [notes, setNotes] = useState<NoteModel[]>([])
@@ -107,8 +108,11 @@ function App() {
 					}}
 				/>
 			)}
-			{true && (
+			{false && (
 				<SignUpModal onDismiss={() => {}} onSignUpSuccessful={() => {}} />
+			)}
+			{false && (
+				<LoginModal onDismiss={() => {}} onLoginSuccessful={() => {}} />
 			)}
 		</Container>
 	)
